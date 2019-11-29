7675570619e5c8da01379509ac602ff3229d4078
aalvarezbretana <aalvarezbretana@danielcastelao.org>
2019-11-29 13:41:30 +0100

    refactor

--- src/factorial/Numeros.java
+++ src/factorial/Numeros.java
@@ -2,58 +2,68 @@ package factorial;
 
 /**
  *
- * @author 
+ * @author
  */
-public class NumerosPrimos {
+public class Numeros {
 
     /**
-     * @param args the command line arguments
-     * arg[0] es el primer parámetro que se le pasa cuando se ejecuta por linea de comandos
      */
     //Codificado por: sAfOrAs
-    //LIstar los numeros según el numero de digitos indicado
-    //Considero solo hasta numeros menores a 100000 (5 digitos), 
-    //por el hecho de k buscar numeros primos a partir de 6 digitos, el proceso se hace muy lento.
-
-    public static boolean p = false;
-    
+    //LIstar los Numeros según el numero de digitos indicado
+    //Considero solo hasta Numeros menores a 100000 (5 digitos), 
+    //por el hecho de k buscar Numeros primos a partir de 6 digitos, el proceso se hace muy lento.
+    public static boolean validez = false;
 
     public static void main(String arg[]) {
-        int nD=0;
-        int nd=0;
-        nD=Integer.parseInt(arg[0]);
-        if(nD<=0)
+        int numeroDigitos1 = 0;
+        int numeroDigitos2 = 0;
+        numeroDigitos1 = Integer.parseInt(arg[0]);
+        if (numeroDigitos1 <= 0) {
             System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
-        for(int i = 1; i <= 99999; i++ )
-        {
-        int div = i;
-        int c=0;
-
-        while (div != 0)
-        {div = div / 10;
-            c++;
         }
-        nd=c;
+        for (int posicion1 = 1; posicion1 <= 99999; posicion1++) {
+            int divisor1 = posicion1;
+            int contador1 = 0;
 
+            while (divisor1 != 0) {
+                divisor1 = divisor1 / 10;
+                contador1++;
+            }
+            numeroDigitos2 = contador1;
 
-        if(nd==nD){
-            if (i < 4) p = true;
-            else
-            {
-                if (i % 2 == 0) p = false;
-                else { int c1 = 0;
-                        int i1 = 1;int l = (i - 1) / 2;if  (l % 2 == 0) l--;
-                        while(i1 <= l) { if (i % i1 == 0) c1++;
-        i1 += 2;
-        if (c1 == 2) i1 = l + 1;
+            if (numeroDigitos2 == numeroDigitos1) {
+                if (posicion1 < 4) {
+                    validez = true;
+                } else {
+                    if (posicion1 % 2 == 0) {
+                        validez = false;
+                    } else {
+                        int contador2 = 0;
+                        int posicion2 = 1;
+                        int divisor2 = (posicion1 - 1) / 2;
+                        if (divisor2 % 2 == 0) {
+                            divisor2--;
+                        }
+                        while (posicion2 <= divisor2) {
+                            if (posicion1 % posicion2 == 0) {
+                                contador2++;
+                            }
+                            posicion2 += 2;
+                            if (contador2 == 2) {
+                                posicion2 = divisor2 + 1;
+                            }
                         }
 
-                        if (c1 == 1) p = true; } }
+                        if (contador2 == 1) {
+                            validez = true;
+                        }
+                    }
+                }
 
-                if (p == true)
-                    System.out.println(i);
-        } } }}
- 
-       
- 
+                if (validez == true) {
+                    System.out.println(posicion1);
+                }
+            }
+        }
+    }
 }
